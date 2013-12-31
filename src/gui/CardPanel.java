7118f606a5c270aab0ba10f011976d9bc1c2ccfc
AndLen <backfiah@gmail.com>
2013-12-31 13:49:55 +1300

    Small fixes with rendering text

--- src/gui/CardPanel.java
+++ src/gui/CardPanel.java
@@ -102,7 +102,7 @@ public class CardPanel extends JPanel implements ComponentListener, MouseListene
         }
         if (indexToStartFrom > 0) {
             g.setColor(Color.white);
-            g.drawString(indexToStartFrom + " cards hidden", (float) X_BOARD_OFFSET, (float) Y_BOARD_OFFSET - 1);
+            g.drawString(indexToStartFrom + indexToStartFrom == 1 ? "card hidden" : "cards hidden", (float) X_BOARD_OFFSET, (float) Y_BOARD_OFFSET - 1);
         }
         y = Y_BOARD_OFFSET;
         x += CARD_WIDTH + CARD_X_GAP;
@@ -118,7 +118,7 @@ public class CardPanel extends JPanel implements ComponentListener, MouseListene
             }
             if (indexToRenderFrom > 0) {
                 g.setColor(Color.white);
-                g.drawString(indexToRenderFrom + " cards hidden", (float) x, (float) y - 1);
+                g.drawString(indexToRenderFrom + indexToStartFrom == 1 ? "card hidden" : "cards hidden", (float) X_BOARD_OFFSET, (float) Y_BOARD_OFFSET - 1);
             }
             y = Y_BOARD_OFFSET;
             x += CARD_WIDTH + CARD_X_GAP;
